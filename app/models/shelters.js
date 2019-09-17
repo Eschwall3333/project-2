7ee176ff4945cdb6602576d2d46e6b084a674c01
Jeffrey Layfield <jlayfield85@gmail.com>
2019-09-16 20:20:08 -0400

    Made API call work in console log and searches now by CITY, not by zip code

--- app/models/shelters.js
+++ app/models/shelters.js
@@ -4,14 +4,7 @@
 var Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to the DB.
 var sequelize = require("../config/connection.js");
-// Creates a "Book" model that matches up with DB
-<<<<<<< HEAD
-var db = sequelize.define("shelters", {
-  title: Sequelize.STRING,
-  author: Sequelize.STRING,
-  genre: Sequelize.STRING,
-  pages: Sequelize.INTEGER
-=======
+// Creates a "shelter" model that matches up with DB
 var Shelter = sequelize.define("Shelter", {
   NAME: Sequelize.STRING,
   ADDRESS: Sequelize.STRING,
@@ -23,7 +16,6 @@ var Shelter = sequelize.define("Shelter", {
   WEBSITE: Sequelize.STRING,
   PET_CODE: Sequelize.STRING,
   TELEPHONE: Sequelize.STRING
->>>>>>> 471a57188e23dd9b92deb3a3d11fd7b9bb79e1aa
 });
 
 // Makes the Book Model available for other files (will also create a table)
