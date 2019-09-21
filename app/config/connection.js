3cb8db712521ba998ae2bbd367cc019073b98437
Emory Schwall <emory3@comcast.net>
2019-09-21 10:00:10 -0400

    more stuff

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,12 +1,15 @@
 //NPM Packages Used//
 //var mysql = require("mysql");
 var Sequelize = require("sequelize");
-
-var sequelize = new Sequelize("usa_shelters_db","root", "Emory123456789987654321", {
-   host: "localhost",
-   port: 3306, 
-   dialect: "mysql"
-});
+var connection = process.env.JAWSDB_URL || {
+   "username": "root",
+        "password": "Emory123456789987654321",
+        "database": "usa_shelters_db",
+        "host": "127.0.0.1",
+        "port": 3306,
+        "dialect": "mysql"
+} 
+var sequelize = new Sequelize(connection);
 //MySQL DB Connection Info and Error Message Handling//
 //Connecting to DB//
 
